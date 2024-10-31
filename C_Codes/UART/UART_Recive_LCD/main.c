90d8fc08b57599110d2378758cb2db88a63d88e8
Vardhan1604 <manivardhan.yeruva3@gmail.com>
2024-10-31 17:42:37 +0530

    update

--- C_Codes/UART/UART_Recive_LCD/main.c
+++ C_Codes/UART/UART_Recive_LCD/main.c
@@ -1,30 +1,20 @@
 #include <reg51.h>
 #include <LCD.h>
 #include <Delay.h>
-sbit sw = P2^0;
+#include <Uart.h>
+
 
 void main() {
-    TMOD = 0x20;        
-    TH1 = 0xFD;          
-    SCON = 0x50;
-    TR1 = 1;           
+	  
+	  Uart_Init(9600);
     LCD_Init();
-	  Sprint("UART IS TO R");
+	  Sprint("UART IS Ready");
 	  Delay(1000);
 	  LCD_Cmd(0x01);
     while(1) {
             if(RI == 1)
 						{							
-                RI = 0;
-                LCD_Data(SBUF);		
-						}
-             if(sw == 0)
-							 {
-								 Delay(250);
-								 SBUF = 'M';
-								 while(TI == 0);  
-                 TI = 0;
-								 
-						 }							 
+                LCD_Data(Uart_Rx());		
+						}		 
             }
         }
